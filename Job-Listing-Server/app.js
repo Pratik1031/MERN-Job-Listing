031a468cd5eb7d6c92870ef456ed0883b2e75a6b
Pratik Dahekar <pratikdahekar7@gmail.com>
2024-01-15 15:44:02 +0530

    Frontend part added

--- Job-Listing-Server/app.js
+++ Job-Listing-Server/app.js
@@ -5,11 +5,23 @@ const cookieParser = require('cookie-parser');
 const { ErrorMiddleware } = require('./middlewares/ErrorMiddleWare');
 const app = express();
 
+const cors = require('cors');
+
 dotenv.config({
   path: './config/.env',
 });
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use('/user', User);
+
 app.use(ErrorMiddleware);
+
 module.exports = app;
--- Job-Listing-Server/controller/UserContoller.js
+++ Job-Listing-Server/controller/UserContoller.js
@@ -15,81 +15,69 @@ const checkRoute = (req, res) => {
 const register = async (req, res, next) => {
   try {
     const { name, email, mobile, password } = req.body;
+
     const user = await userCollection.findOne({ email });
+
     if (user) {
-      return next(new ErrorHandler('user alrready exists', 409));
+      return next(new ErrorHandler('user already exists', 409));
     }
 
     const hashed_password = await bcrypt.hash(password, 10);
+
     const created_user = await userCollection.create({
       name,
       email,
       mobile,
       password: hashed_password,
     });
-    return setCookie(res, created_user, 201, `${name} registered successfully`);
+
+    const words = name.split(' ');
+    const trimmedName = words.slice(0, 1).join(' ');
+
+    return setCookie(
+      res,
+      created_user,
+      201,
+      `${trimmedName} registered successfully`
+    );
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      success: true,
-      message: error.message,
-    });
+    next(new ErrorHandler(error.message, 500));
   }
 };
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     const user = await userCollection.findOne({ email }).select('+password');
+
     if (!user) {
-      return next(new ErrorHandler('User not found', 404));
+      return next(new ErrorHandler('Invalid email or password', 404));
     }
 
     const password_match = await bcrypt.compare(password, user.password);
+
+    const words = user.name.split(' ');
+    const trimmedName = words.slice(0, 1).join(' ');
+
     if (password_match) {
-      return setCookie(res, user, 200, `Welcome ${user.name}`);
+      return setCookie(res, user, 200, `Welcome ${trimmedName}`);
     }
 
-    return next(new ErrorHandler('Invaild Email or Password', 404));
+    return next(new ErrorHandler('Invalid email or password', 404));
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      success: true,
-      message: error.message,
-    });
+    next(new ErrorHandler(error.message, 500));
   }
 };
 
-const addJob = async (req, res) => {
+const addJob = async (req, res, next) => {
   try {
-    const {
-      company_name,
-      company_logo,
-      job_position,
-      monthly_salary,
-      job_type,
-      remote_office,
-      location,
-      job_description,
-      about_company,
-      skills,
-      information,
-    } = req.body;
-
-    console.log(req.user._id);
-
     const job = await jobCollection.create({
-      company_name,
-      company_logo,
-      job_position,
-      monthly_salary,
-      job_type,
-      remote_office,
-      location,
-      job_description,
-      about_company,
-      skills,
-      information,
+      ...req.body,
+      job_position: req.body.job_position.toLowerCase(),
+      skills: req.body.skills
+        .split(/\s*,\s*/)
+        .map((skill) => skill.trim().toLowerCase()),
       user: req.user._id,
     });
 
@@ -105,45 +93,23 @@ const addJob = async (req, res) => {
 const updateJob = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const {
-      company_name,
-      company_logo,
-      job_position,
-      monthly_salary,
-      job_type,
-      remote_office,
-      location,
-      job_description,
-      about_company,
-      skills,
-      information,
-    } = req.body;
+    const { user } = req;
 
-    const updatedFields = {
-      company_name,
-      company_logo,
-      job_position,
-      monthly_salary,
-      job_type,
-      remote_office,
-      location,
-      job_description,
-      about_company,
-      skills,
-      information,
-    };
+    const skillsArray =
+      typeof req.body.skills === 'string'
+        ? req.body.skills
+            .split(/\s*,\s*/)
+            .map((skill) => skill.trim().toLowerCase())
+        : req.body.skills;
 
-    const updatedJob = await jobCollection.findOneAndUpdate(
-      { _id: id },
-      { $set: updatedFields },
+    const updatedJob = await jobCollection.findByIdAndUpdate(
+      id,
+      { ...req.body, skills: skillsArray, user: user._id },
       { new: true }
     );
-    console.log(updateJob);
+
     if (!updatedJob) {
-      return res.status(404).json({
-        success: false,
-        message: 'Job not found',
-      });
+      return next(new ErrorHandler('Job Not Found', 404));
     }
 
     res.status(200).json({
@@ -151,28 +117,96 @@ const updateJob = async (req, res, next) => {
       message: 'Job updated successfully',
     });
   } catch (error) {
-    next(new ErrorHandler('Internal server error', 500));
+    next(new ErrorHandler(error.message, 500));
   }
 };
 
-const getSpecificJob = async (req, res, next) => {
+const searchJob = async (req, res, next) => {
   try {
     const { job_position, skills } = req.body;
 
-    const jobs = await jobCollection.find({
-      $or: [
-        { job_position: { $eq: job_position } },
-        { skills: { $in: skills } },
-      ],
-    });
+    let query = {};
+
+    if (skills.length > 0 && job_position === '') {
+      query = {
+        skills: { $in: skills },
+      };
+    } else {
+      query = {
+        $or: [
+          { job_position: new RegExp(job_position, 'i') },
+          { skills: { $in: skills } },
+        ],
+      };
+    }
+
+    const jobs = await jobCollection.find(query).select('-user');
 
     if (!jobs) {
-      next(new ErrorHandler('Jobs not found', 404));
+      return next(new ErrorHandler('Jobs not found', 404));
     }
     res.status(200).json({
       success: true,
       jobs,
     });
+  } catch (error) {
+    next(new ErrorHandler(error.message, 500));
+  }
+};
+
+const getSpecificJob = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const job = await jobCollection.findById(id).select('-user');
+
+    if (!job) {
+      return next(new ErrorHandler('Jobs not found', 404));
+    }
+
+    res.status(200).json({
+      success: true,
+      job,
+    });
+  } catch (error) {
+    next(new ErrorHandler(error.message, 500));
+  }
+};
+
+const getAllJobs = async (req, res, next) => {
+  const jobs = await jobCollection.find({});
+  if (!jobs) {
+    return next(new errorMiddleware('No Jobs Found', 404));
+  }
+  res.status(200).json({
+    success: true,
+    jobs,
+  });
+};
+
+const getMyProfile = async (req, res, next) => {
+  const profile = await userCollection.findById(req.user._id).select('-_id');
+  if (!profile) {
+    return next(new errorMiddleware('Profile not found', 404));
+  }
+  res.status(200).json({
+    success: true,
+    profile,
+  });
+};
+
+const logout = (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .cookie('token', '', {
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === 'Development' ? 'lax' : 'none',
+        secure: process.env.NODE_ENV === 'Development' ? false : true,
+      })
+      .json({
+        success: true,
+        message: 'logged out',
+      });
   } catch (error) {
     next(new ErrorHandler(error.message, 500));
   }
@@ -184,5 +218,9 @@ module.exports = {
   login,
   addJob,
   updateJob,
+  searchJob,
   getSpecificJob,
+  logout,
+  getAllJobs,
+  getMyProfile,
 };
